970a418734d41fb70b45d779544f26a30c2467b2
hannah-becker <becker1123@proton.me>
2026-03-09 15:26:37 +0100

    append elements not in official list

--- plants.R
+++ plants.R
@@ -4,6 +4,7 @@
 #requirements ----
 library(readxl)
 library(writexl)
+library(dplyr)
 
 #housekeeping ----
 #uncomment and run if necessary
@@ -25,7 +26,8 @@ list <- readxl::read_excel("./data/test_data.xlsx") #can read xlsx and xls files
 index <- pmatch(list[[1]], offlist[[3]]) # if you're not interested in partial matches (e.g. Draba verna returning a match for Draba verna agg. in the official list) you can run this as match(...) instead
 
 matches <- offlist[na.omit(index), ]
-print(paste0("not matched:", list[which(is.na(index)), ])) #all species listed here were not found on the official list. If the official list is comprehensive, check these for spelling and potential taxonomy changes and rerun if necessary
+missing <- list[which(is.na(index)), ][[1]]
+print(paste0("not matched: ", missing)) #all species listed here were not found on the official list. If the official list is comprehensive, check these for spelling and potential taxonomy changes and rerun if necessary
 
 
 m <- grepl("\\.|\\s[x]\\s|(\\w+\\s(\\w+|\\w+\\-\\w+)\\s\\w+)", matches[[3]],  perl = TRUE)
@@ -33,7 +35,11 @@ m <- grepl("\\.|\\s[x]\\s|(\\w+\\s(\\w+|\\w+\\-\\w+)\\s\\w+)", matches[[3]],  pe
 #finds names in which . has been used (catches s.str., s.l., agg., subsp.), " x " has been used (catches hybrids) or in which the taxon name consists of more than just genus and species epithet (catches subspecies that were not explicitly declared)
 #all of the above are then flagged for manual verification. 
 
-matches <- cbind(manual_check_recommended = m, matches)
+matches <- cbind(manual_check_recommended = m, not_found = "", matches)
+
+#add not-found species back to the list:
+
+matches <- rows_insert(matches, tibble(not_found = missing, manual_check_recommended = TRUE))
 
 #as it stands rn it cannot handle to-be-matched lists in which species are listed with proper authority a la Hyacinthoides italica (L.) Rothm.
 #if that is a feature we might need lmk and I'll see about implementing that
